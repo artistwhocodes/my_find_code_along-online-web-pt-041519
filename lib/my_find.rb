13ff573cc2edac19feb0b9acf071a22159b52ad7
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-21 03:41:24 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def my_find(collection)
-  counter = first
+  counter = 1
   while counter < collection.length
     return(collection[counter]) if yield(collection[counter])
      counter = counter.next
